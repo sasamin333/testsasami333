d9315da136ffcd88d8030b08e6155a266003663f
Ryutaro Nakajima <Turuwatari@yahoo.co.jp>
2016-05-25 17:50:00 +0900

    ユーザーからメイン画面へ戻る為のアクションクラスを作成しました。

---
+++ src/com/internousdev/prototype1605/action/UserBackAction.java
@@ -0,0 +1,13 @@
+package com.internousdev.prototype1605.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserBackAction extends ActionSupport{
+
+	private static final long serialVersionUID = 651434533244866952L;
+
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
