0bf0dc2f8a347f043204db99a60c084684f71ec1
Taiki Miyajima <9aba1104@gmail.com>
2016-05-25 14:53:46 +0900

    DBConnectorを作成しました

---
+++ src/com/internousdev/prototype1605/util/DBConnector.java
@@ -0,0 +1,35 @@
+package com.internousdev.prototype1605.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector
+{
+	private static String driverName = "com.mysql.jdbc.Driver";
+
+	private static String url = "jdbc:mysql://localhost/prototype1605";
+
+	private static String user = "root";
+
+	private static String pass = "mysql";
+
+	public Connection getConnection()
+	{
+		Connection con = null;
+		try
+		{
+			Class.forName(driverName);
+			con = DriverManager.getConnection(url,user,pass);
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
